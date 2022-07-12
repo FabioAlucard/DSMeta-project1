b7c47000b940d3747259c9681c2d81eeb47884f0
FabioAlucard <fabio.fm82@hotmail.com>
2022-07-12 18:20:00 -0300

    Created Backend and Frontend

---
+++ backend/src/main/java/com/devsuperior/dsmetat1/Dsmetat1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmetat1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsmetat1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsmetat1Application.class, args);
+	}
+
+}
